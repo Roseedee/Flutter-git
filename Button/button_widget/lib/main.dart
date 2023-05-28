18fe327016bf3def5719f97ea063d22c58056aae
Roseedee Cehleah <roseedee2002@gmail.com>
2023-05-28 08:01:13 +0700

    update

--- Button/button_widget/lib/main.dart
+++ Button/button_widget/lib/main.dart
@@ -28,28 +28,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
-              onPressed: (){
-                print('Button clicked');
-              },
-              child: const Text("Elevated Button"),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ) // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print('Button clicked');
+                },
+                child: const Text("Elevated Button"),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: const Text("Elevated button has icon"),
+                icon: const Icon(Icons.add),
+              ),
+              OutlinedButton(
+                child: const Text("Outlined Button"),
+                onPressed: () {},
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                label: const Text("Outlined button has icon"),
+                icon: const Icon(Icons.add),
+              ),
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {},
+              ),
+              TextButton(
+                child: const Text("Text Button"),
+                onPressed: () {},
+              ),
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text('Text button has icon'))
+            ],
+          ),
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
